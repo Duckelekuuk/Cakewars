333de131137844a9230a7b06fcea11b3a999476d
Duco Lindner <duckelekuuk@Gmail.com>
2017-04-11 15:44:28 +0200

    Updated gameplayer to run with abstractTeam

--- src/main/java/com/duckelekuuk/cakewars/match/GamePlayer.java
+++ src/main/java/com/duckelekuuk/cakewars/match/GamePlayer.java
@@ -1,16 +1,15 @@
 package com.duckelekuuk.cakewars.match;
 
-import com.duckelekuuk.cakewars.match.teams.ITeam;
+import com.duckelekuuk.cakewars.match.teams.AbstractTeam;
 import lombok.Getter;
 import lombok.Setter;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.Team;
 
 @Getter
 public class GamePlayer {
 
     private Player player;
-    private @Setter ITeam team;
+    private @Setter AbstractTeam team;
 
     private @Setter boolean spectator;
 
